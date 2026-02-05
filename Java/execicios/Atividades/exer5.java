780c9484f4696c7f1aff0aa6999d83da34f5286e
joao-henrique-senac <joao.henrique.14.03.2007@gmail.com>
2026-02-04 21:51:20 -0300

    exercicios de distancia atualizado

--- Java/execicios/Atividades/exer5.java
+++ Java/execicios/Atividades/exer5.java
@@ -7,22 +7,9 @@ public class exer5 {
         //distância dada por: dist = {(x2 - x1)² + (y2 - y1)²}^(1/2)
         //Atividade 1: realizar o calculo dentro de um metodo.
         //static void calcularDistancia(double x1, double y1, double x2, double y2){}
+      
+        calcularDistancia(2, 3, 5, 7);
         
-       double x1,x2,y1,y2,distX,distY,dx,dy,resultado;
-       x1 = 0;
-       y1 = 0;
-       x2 = 3;
-       y2 = 4;
-       dx = x2 - x1;
-       dy = y2 - y1;
-       distX = dx * dx;
-       distY = dy * dy;
-       System.out.println("Distancia em X ao quadrado: " + distX);
-       System.out.println("Distancia em Y ao quadrado: " + distY);
-       resultado = Math.sqrt(distX + distY);
-       System.out.println("Distância entre os pontos: " + resultado);
-         calcularDistancia(0,0,3,4);
-         
     }
        public static void calcularDistancia(double x1, double y1, double x2, double y2){
         double dx = x2 - x1;
